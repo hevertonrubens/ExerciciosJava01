bccfde5a818ae8a6bfa3d50f9e312bf101316109
Your Name <you@example.com>
2019-09-22 21:27:03 -0300

    Exercício 01

--- untitled/src/com/unama/LetraJ.java
+++ untitled/src/com/unama/LetraJ.java
@@ -9,7 +9,7 @@ public class LetraJ {
         double totalemdolar;
         System.out.println("Informe um valor: R$");
         real = sc.nextFloat();
-        totalemdolar = real/4.15;
+        totalemdolar = real*4.15;
         System.out.println("O valor em dólar será de: U$S" +totalemdolar);
     }
 }
